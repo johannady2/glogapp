3f223dbf323b0f5156e3c810c8281448f2edbeee
Johanna Cristine Dy <johannady2@gmail.com>
2015-05-17 15:43:06 +1100

    okay fixed scan/search/catalogue

--- www/js/navClickedListener.js
+++ www/js/navClickedListener.js
@@ -4,9 +4,10 @@ $(document).on('navClicked',function(event,filename)
     if(filename == "catalogue.html")
     { 
         if (navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|IEMobile)/))
-        { 
+        { globalorderFromSwitch = 0;
+        alert('switch =='+ globalorderFromSwitch );
             
-            db.transaction(queryCatalogues, errorCB);
+         db.transaction(queryCatalogues, errorCB);
             
 
                 //off click is fix for event trigger multiple times.There are many other solutions if needed to change this in the future.
@@ -25,7 +26,9 @@ $(document).on('navClicked',function(event,filename)
 
     }
     else if(filename == "search.html")
-    {
+    {	globalorderFromSwitch = 1;
+        alert('switch =='+ globalorderFromSwitch );
+	
            
 			db.transaction(queryCategoriesForSelectBox, errorCB);
 			db.transaction(queryCataloguesForSelectBox, errorCB);
